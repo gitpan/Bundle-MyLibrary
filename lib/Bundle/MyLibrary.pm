c0411706528feecdf3bb8854328de04ba7be5673
Ed Summers <ehs@pobox.com>
2003-02-21 18:19:41 +0000

    Import of ESUMMERS/Bundle-MyLibrary-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-MyLibrary
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ESUMMERS/Bundle-MyLibrary-0.01.tar.gz
    gitpan-cpan-author:       ESUMMERS
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/MyLibrary.pm
@@ -0,0 +1,48 @@
+package Bundle::MyLibrary;
+
+$VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME 
+
+Bundle::MyLibrary - MyLibrary Module Dependencies
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::MyLibrary>
+
+=head1 DESCRIPTION
+
+These are Perl dependencies for the MyLibrary portal software.
+L<http://dewey.library.nd.edu/mylibrary/>
+
+=head1 CONTENTS
+
+CGI
+Bundle::DBI
+Pod::Parser
+Data::ShowTable
+DBD::mysql
+URI
+HTML::Parser
+Bundle::libnet
+Bundle::LWP
+Digest
+Digest::MD5
+MIME::Base64
+MIME::Decoder::Base64
+Mail::Send
+IO::Stringy
+MIME-tools
+Text::Tabs+Wrap
+Mail::Send
+Time::CTime
+Time::ParseDate
+
+=head1 AUTHOR
+
+Ed Summers e<lt>ehs@pobox.come<gt>
+
